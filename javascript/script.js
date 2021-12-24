04ad4d5817730f3a8b2b777d55e0cdfb645eb987
Sebastian Ojefors <godnoken@gmail.com>
2021-12-24 16:28:37 +0100

    Add Book constructor

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,17 @@
-let myLibrary = ["Book1", "Book2", {title: "Lord Of The Rings", author: "J.R.R. Tolkien"}];
+let myLibrary = [{title: "Lord Of The Rings", author: "J.R.R. Tolkien"}];
+
+function Book(title, author) {
+    this.title = title
+    this.author = author
+}
+
+const STARWARS = new Book("Star Wars", "George Lucas");
+
+myLibrary.push(STARWARS);
+
+function addBook() {
+
+}
 
 const body = document.body;
 
