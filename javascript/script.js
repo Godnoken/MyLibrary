6ed7c8468d2ed2bcf4d9647fab8b3383cdcb7072
Sebastian Ojefors <godnoken@gmail.com>
2021-12-25 15:15:24 +0100

    Add addBook function, comments and a form

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,56 @@
 let myLibrary = [{title: "Lord Of The Rings", author: "J.R.R. Tolkien"}];
 
-function Book(title, author) {
+const body = document.body;
+
+function Book(title, author, pages, read = false) {
     this.title = title
     this.author = author
+    this.pages = pages
+    this.read = read
 }
 
-const STARWARS = new Book("Star Wars", "George Lucas");
+const STARWARS = new Book("Star Wars", "George Lucas", "351", "false");
 
 myLibrary.push(STARWARS);
 
-function addBook() {
+const addBookButton = document.createElement("button");
+addBookButton.style.width = "125px";
+addBookButton.style.height = "35px";
+addBookButton.textContent = "Add Book";
 
+body.appendChild(addBookButton);
+
+const bookForm = document.querySelector("#bookForm")
+const bookFormSubmit = document.querySelector("#bookFormSubmit")
+const bookTitle = document.querySelector("#title")
+const bookAuthor = document.querySelector("#author")
+const bookPages = document.querySelector("#pages")
+const bookRead = document.querySelector("#read")
+
+// Adds a new book when pressing submit
+bookFormSubmit.addEventListener("click", addBook);
+
+function addBook(event) {
+
+    // Prevents reloading of the page
+    event.preventDefault();
+
+    // Adds new book to array
+    const bookToAdd = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked);
+    myLibrary.push(bookToAdd);
+
+    // Creates card for the new book added to the array and renders it on the page
+    let book = myLibrary[myLibrary.length - 1];
+    const card = document.createElement("div");
+    card.textContent = `${book.title} ${book.author} ${book.pages} ${book.read}`;
+    body.appendChild(card);
 }
 
-const body = document.body;
-
+// Creates card for all the books stored in the myLibrary array and renders them to the page
 function displayBooks() {
     myLibrary.map(book => {
         const card = document.createElement("div")
-        card.textContent = book.title + " " + book.author
+        card.textContent = `${book.title} ${book.author} ${book.pages} ${book.read}`
         body.appendChild(card)
     })
 }
