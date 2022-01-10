eee04a2c9935a4503a313c6c409db09c0b6b8437
Sebastian Ojefors <godnoken@gmail.com>
2022-01-10 19:40:04 +0100

    Bugfix - Read checkbox now works again

--- javascript/handleIsReadCheckBox.js
+++ javascript/handleIsReadCheckBox.js
@@ -1,16 +1,16 @@
-import { myLibraryArray } from "./main.js";
+import { myLibraryArray, } from "./main.js";
 import { googleBooksArray } from "./displayBooks.js";
 import { saveToLocalStorage } from "./saveToLocalStorage.js";
 
-// Reads checkbox for checked or not checked and sets the book to read or not read in the myLibraryArray, googleBooksArray array
+// Reads checkbox for checked or not checked and sets the book to read or not read in the myLibraryArray array
 export function handleIsReadCheckbox(book) {
 
     const bookTarget = book.target.parentElement.parentElement.parentElement;
     const checkbox = book.target;
 
     if (googleBooksArray.length === 0) {
-        if (checkbox.checked === false) myLibraryArray, googleBooksArray[bookTarget.dataset.bookindex].read = false;
-        else myLibraryArray, googleBooksArray[bookTarget.dataset.bookindex].read = true;
+        if (checkbox.checked === false) myLibraryArray[bookTarget.dataset.bookindex].read = false;
+        else  myLibraryArray[bookTarget.dataset.bookindex].read = true;
     }
 
     saveToLocalStorage();
